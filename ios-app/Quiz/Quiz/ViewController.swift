4d115c1fce249c46baee5328d6ccecc130620f48
Дарья Саитова <dashasaitova1202@mail.ru>
2025-09-23 12:19:56 +0300

    Добавлен проект Quiz в ios-app на ветке ios-development

---
+++ ios-app/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Дарья Саитова on 23.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Quiz"
+    }
+
+
+}
+
